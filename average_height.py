05f22ccf67b24c0d21c097a83825e2d94b779184
Akash Singhal <101013452+Akashsinghal1234@users.noreply.github.com>
2022-10-19 00:25:33 +0530

    Day_5_codes

---
+++ average_height.py
@@ -0,0 +1,22 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+print(student_heights)
+# 🚨 Don't change the code above 
+
+#Write your code below this row 👇
+total_height = 0
+for height in student_heights:
+    total_height += height
+print(total_height)
+
+total_student = 0
+for student in student_heights:
+    total_student += 1
+print(total_student)
+
+a = total_height/total_student
+print(round(a))
+
+
---
+++ sum_of_even_no.py
@@ -0,0 +1,5 @@
+#Write your code below this row 👇
+a = 0
+for i in range (2,101,2):
+    a = a + i
+print(a)
